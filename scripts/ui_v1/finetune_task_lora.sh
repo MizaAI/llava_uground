c97fa666205610f118e62e0e0f79595fe6816e06
deepakzambre <dz@no-reply.com>
2024-11-23 11:16:29 -0800

    Fix PYTHONPATH to find llava module for training

--- scripts/ui_v1/finetune_task_lora.sh
+++ scripts/ui_v1/finetune_task_lora.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export PYTHONPATH=.:$
 deepspeed llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero3.json \
